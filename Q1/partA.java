5aed7b1af0dfa8f88ff584a7a37833fdce205f2f
sdfjsdlsdkfjsls <AndrewM446@nycstudents.net>
2025-05-12 11:49:11 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,1 +1,10 @@
+public int walkDogs(int hour) {
+  int dawgs;
+  if (company.availableDogs(hour) >= maxDogs)
+    { dawgs = maxDogs; }
+  else
+    { dawgs = company.availableDogs(hour); }
 
+  company.updateDogs(hour, dawgs);
+  return dawgs;
+}
