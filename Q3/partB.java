e46d081b418bc75683e11ddb2d3b19f893f909e8
sdfjsdlsdkfjsls <AndrewM446@nycstudents.net>
2025-05-16 00:06:34 -0400

    Update partB.java

--- Q3/partB.java
+++ Q3/partB.java
@@ -1,3 +1,11 @@
 public ArrayList<Match> buildMatches() {
-  
+  ArrayList<Match> list = new ArrayList<Match>();
+  int odd = 0;
+  if (!(competitorList.size() % 2 == 0)) {
+    odd++;
+  }
+  for (int i = 0; i < competitorList.size(); i++) {
+    list.add(new Match(competitorList.get(i + odd), competitorList.get(i-competitorList.size()-1));
+  }
+  return list;
 }
