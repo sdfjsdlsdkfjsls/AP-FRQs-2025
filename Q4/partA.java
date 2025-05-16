993f10e1c99541ce706621dfc609b4c8f18f9233
sdfjsdlsdkfjsls <AndrewM446@nycstudents.net>
2025-05-16 00:18:39 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,1 +1,8 @@
-
+public SumOrSameGame(int numRows, int numCols) {
+  puzzle = new int[numRows][numCols];
+  for (int row = 0; row < numRows; row++) {
+    for (int col = 0; col < numCols; col++) {
+      puzzle[row][col] = (int)(Math.random()*10+1);
+    }
+  }
+}
