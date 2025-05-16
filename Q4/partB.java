96626e4cb27eb271147c4af8970a1e4f69eb3026
sdfjsdlsdkfjsls <AndrewM446@nycstudents.net>
2025-05-16 00:46:00 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -1,9 +1,9 @@
 public boolean clearPair(int row, int col) {
   for (int i = row; i < puzzle.length; i++) {
     for (int j = 0; j < puzzle[0].length; j++) {
-      int first = puzzle[row][col];
-      int second = puzzle[i][j];
-      if (first == second || first + second == 10) {
+      int current = puzzle[row][col];
+      int target = puzzle[i][j];
+      if (!(i == row && j == col) && (first == second || first + second == 10)) {
         puzzle[row][col] = 0;
         puzzle[i][j] = 0;
         return true;
