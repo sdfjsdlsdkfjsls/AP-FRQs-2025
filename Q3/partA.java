475af94a5eb88e9bb5c9b520fffb04f3c77f1945
sdfjsdlsdkfjsls <AndrewM446@nycstudents.net>
2025-05-15 23:22:29 -0400

    Update partA.java

--- Q3/partA.java
+++ Q3/partA.java
@@ -1,1 +1,9 @@
-
+public Round(String[] names) {
+  ArrayList<Competitor> list = new ArrayList<Competitor>();
+  int rank = 1;
+  for (String name: names) {
+    list.add(new Competitor(name, rank));
+    rank++;
+  }
+  return list;
+}
